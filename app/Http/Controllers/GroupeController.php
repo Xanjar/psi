08317b85ec3eed50d0c0bde4b3e3c88b47697854
Sofian <sofian.khalf1@gmail.com>
2020-03-19 12:45:11 +0100

    installation fini

---
+++ app/Http/Controllers/GroupeController.php
@@ -0,0 +1,49 @@
+namespace App\Http\Controllers;
+<?php
+use App\Http\Controllers\Controller;
+use App\Groupe;
+
+class GroupeController extends Controller
+{
+    /**
+     * Show all groups.
+     *
+     * @param
+     * @return View
+     */
+    public function show()
+    {
+        // to do
+    }
+
+
+    /**
+     * Create a group
+     *
+     * @param
+     * @return View
+     */
+    public function create() {
+        // to do
+    }
+
+    /**
+     * Show a group
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function modify($idGroup) {
+        // to do
+    }
+
+    /**
+     * Delete a group
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function delete($idGroup) {
+        // to do
+    }
+}
